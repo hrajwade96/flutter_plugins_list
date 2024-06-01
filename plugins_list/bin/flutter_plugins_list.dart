f3db6d8f3d2ee5d331b087cbc5c0a8c3c11e2f69
hrishikesh <hrishikesh.rajwade@tatadigital.com>
2024-06-01 09:50:36 +0530

    csv and json support added, example package added.

--- plugins_list/bin/flutter_plugins_list.dart
+++ plugins_list/bin/flutter_plugins_list.dart
@@ -1,5 +1,4 @@
 library flutter_plugins_list;
-// ignore_for_file: avoid_dynamic_calls
 
 import 'dart:async';
 import 'dart:convert';
@@ -13,33 +12,41 @@ import 'package:yaml/yaml.dart';
 
 const projectPackage = 'project package';
 
+Future<String> findGitRepositoryRoot() async {
+  try {
+    var result = await Process.run('git', ['rev-parse', '--show-toplevel']);
+    if (result.exitCode != 0) {
+      throw Exception('git command failed: ${result.stderr}');
+    }
+    return result.stdout.toString().trim();
+  } on ProcessException catch (e) {
+    throw Exception('Failed to run git command: $e');
+  }
+}
+
 Future<String> _fetchLatestVersion(
     String packageName, String versionInfo) async {
-  print('processing $packageName version info is $versionInfo');
   final url = Uri.parse('https://pub.dev/api/packages/$packageName');
   try {
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final jsonResponse = jsonDecode(response.body);
-      print('Fetched latest version from pub.dev $packageName Response '
-          '${jsonResponse['latest']['version'].toString()}');
       return jsonResponse['latest']['version'].toString();
     } else {
-      return 'could not fetch, this could be a project package';
+      return 'Could not fetch, this could be a project package';
     }
   } catch (e) {
-    return 'error';
+    return 'Error';
   }
 }
 
-bool _shouldIgnore(String filePath, List<String> pathsToIgnore) =>
+bool shouldIgnore(String filePath, List<String> pathsToIgnore) =>
     pathsToIgnore.any((ignorePattern) {
-      // Using Glob from 'package:glob/glob.dart' to match patterns
       final glob = Glob(ignorePattern);
       return glob.matches(filePath);
     });
 
-Future<void> main() async {
+Future<void> findDependencies() async {
   final repoRoot = await findGitRepositoryRoot();
   final glob = Glob(path.join(repoRoot, '**/pubspec.yaml'));
 
@@ -47,19 +54,11 @@ Future<void> main() async {
   var versionInfo = '';
   final allDependencies = <String, Set<String>>{};
 
-  print('Searching for pubspec.yaml files...');
-
-  var fileCount = 0;
-
   await for (final FileSystemEntity entity in glob.list(followLinks: false)) {
-    // Convert the file path to a relative path from the current directory
     final relativeFilePath = path.relative(entity.path, from: path.current);
-    // Normalize the file path to ensure consistency in path format
     final normalizedFilePath = path.normalize(relativeFilePath);
 
-    if (entity is File && !_shouldIgnore(normalizedFilePath, pathsToIgnore)) {
-      fileCount++;
-      print('Processing ${entity.path}...');
+    if (entity is File && !shouldIgnore(normalizedFilePath, pathsToIgnore)) {
       try {
         final pubspecContent = await entity.readAsString();
         final doc = loadYaml(pubspecContent);
@@ -70,7 +69,6 @@ Future<void> main() async {
             dependencies.forEach((key, value) {
               final packageName = key.toString();
               versionInfo = projectPackage;
-              // Default value if the version is not specified
               if (value == null) {
                 versionInfo = 'no version specified';
               } else if (value is String) {
@@ -95,42 +93,47 @@ Future<void> main() async {
     }
   }
 
-  print('Processed $fileCount pubspec.yaml files.'
-      ' Fetching latest versions...');
-
   final latestVersions = <String, String>{};
   for (var packageName in allDependencies.keys) {
     final latestVersion = await _fetchLatestVersion(
         packageName, allDependencies[packageName].toString());
     latestVersions[packageName] = latestVersion;
   }
-  print('Writing to dependencies_list.txt...');
 
-  final file = File('dependencies_list.txt');
-  final sink = file.openWrite()
-    ..writeln(
-        'Dependencies and their versions/details found across all pubspec.yaml files, including latest versions from pub.dev:');
+  await generateCsvFile(allDependencies, latestVersions);
+  await generateJsonFile(allDependencies, latestVersions);
+}
+
+Future<void> generateCsvFile(Map<String, Set<String>> allDependencies,
+    Map<String, String> latestVersions) async {
+  final csvFile = File('dependencies_list.csv');
+  final sink = csvFile.openWrite();
+  sink.writeln('Dependency Name,Current Version(s),Latest Version');
+
   allDependencies.forEach((packageName, versions) {
     final latestVersion = latestVersions[packageName] ?? projectPackage;
-    sink.writeln('$packageName: ${versions.join(', ')} '
-        '(Latest as of now: $latestVersion)');
+    sink.writeln('$packageName,${versions.join(' ')},$latestVersion');
   });
 
   await sink.flush();
   await sink.close();
-
-  print(
-      'Completed. Dependencies list has been written to dependencies_list.txt');
 }
 
-Future<String> findGitRepositoryRoot() async {
-  try {
-    var result = await Process.run('git', ['rev-parse', '--show-toplevel']);
-    if (result.exitCode != 0) {
-      throw Exception('git command failed: ${result.stderr}');
-    }
-    return result.stdout.toString().trim();
-  } on ProcessException catch (e) {
-    throw Exception('Failed to run git command: $e');
-  }
+Future<void> generateJsonFile(Map<String, Set<String>> allDependencies,
+    Map<String, String> latestVersions) async {
+  final jsonFile = File('dependencies_list.json');
+  final jsonContent = <String, dynamic>{};
+
+  allDependencies.forEach((packageName, versions) {
+    jsonContent[packageName] = {
+      'current_versions': versions.toList(),
+      'latest_version': latestVersions[packageName] ?? projectPackage,
+    };
+  });
+
+  await jsonFile.writeAsString(jsonEncode(jsonContent));
+}
+
+Future<void> main() async {
+  await findDependencies();
 }
---
+++ plugins_list/example/demo_package/README.md
@@ -0,0 +1,7 @@
+This is a demo package for demonstrating how to use plugins_list tool.
+Run below commands to generate the output files.
+
+```sh 
+flutter_plugins_list
+```
+The output will be generated under outputs folder.
